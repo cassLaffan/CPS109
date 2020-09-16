c0c3ee14000a1aad87de5bc467bae49d99d38ac3
cassLaffan <poisonberryvines@gmail.com>
2020-09-16 15:03:27 -0400

    Adding the first set of examples

---
+++ Lab1/Example_One.py
@@ -0,0 +1,14 @@
+# Our very first python example!
+# Adding the pound sign (hashtag) in front of a line comments it out
+# That means that when the machine reads your code, it will ignore everything
+# on the line after the hashtag.
+
+# To "run" this program (in a text editor such as VSCode or in your terminal/command line)
+# type in "Example_One.py" and hit Enter. It won't read out any of the text printed out.
+
+print("This is how you print a line")
+
+# Notice how after the word print the line we actually want to print is in brackets?
+# What is in those brackets is called an "Argument".
+# An argument is what you put in a function to make it do what you like.
+# In Python 3 (the version we're working in), print is a function.
---
+++ Lab1/Example_Two.py
@@ -0,0 +1,14 @@
+# Now let's do the classic opening program whenever we learn a new language.
+# Let's tell the computer to print "Hello, world!" to our terminal.
+# Remember: the terminal is that funky looking window/blinking cursor where your text
+# is fed out.
+
+print("Hello, world!\n")
+
+# Notice the '\n' at the end of the line? That's how the computer knows to go to the
+# next line of text. That way, you don't have everything printing off on one line!
+# It is known as the "newline character".
+
+# However, when you run this program, you'll notice that it will skip a line, 
+# leaving a blank line after our "Hello, world!" output.
+# That's because the print function in Python 3 automatically puts a newline character at the end of strings.
