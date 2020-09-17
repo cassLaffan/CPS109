cdf8c9a1d0f50157e833fc11d9c1ba6035eb7f3e
cassLaffan <poisonberryvines@gmail.com>
2020-09-17 09:54:11 -0400

    Removing the answer so that the students don't just open github and see it. Don't worry, I have it saved locally and will post after.

---
+++ Lab1/Example_Seven.py
@@ -0,0 +1,18 @@
+# One more example. Let's deal with OR and AND
+
+# These just check the true status of whatever is on either side of the operator.
+
+print(True and False) # Will return False because the second condition is false!
+
+print(True and True) # Will return True because both conditions are true!
+
+# You can assign these to variables like any other datatype.
+x = False
+y = True
+
+# Do you know what this will print off?
+print((x and y) or (y and y))
+
+# You can, in principle, string as many of these together as your heart desires.
+
+print(x or y and y or x) # Hehe you just have to keep your logic in check. I would reccomend using brackets.
