be2b06828a0ac4dd0d8d44678029280ff5399ac5
cassLaffan <poisonberryvines@gmail.com>
2020-09-23 17:15:41 -0400

    Adding a third example

---
+++ Lab2/ExampleThree.py
@@ -0,0 +1,15 @@
+# Let's look at a for loop in a function!
+
+# For loops can work anywhere in a program so long as they're correctly written
+def for_loop_here(num):
+    x = 1
+    for i in range(num):
+        x+=i
+    return x
+
+if __name__ == "__main__":
+    item = 6
+    # Remember, you can skip extra lines by using the function and its argument(s)
+    # as arguments for other functions, like print!
+    # Also remember that you have to cast it to a string if you want to concatonate a string this way
+    print("Our number is: " + str(for_loop_here(item)))
