d6beb5d6778b7e21f84da25ac6efe92e22150e91
cassLaffan <poisonberryvines@gmail.com>
2020-10-07 22:58:24 -0400

    Making the example a bit longer

--- Lab4/ExampleOne.py
+++ Lab4/ExampleOne.py
@@ -1,10 +1,12 @@
-
+# This example is just to remind you guys how functions are called in Python. 
 def backward_string(my_string):
     new_string = my_string[::-1]
-
     return new_string
 
-
+# This shows a function that takes two arguments
+def multiply_string(my_string, mulitplier):
+    newer_string = my_string * mulitplier
+    return newer_string
 
 if __name__ == "__main__":
     
@@ -12,6 +14,9 @@ if __name__ == "__main__":
 
     while(inp != ""):
         inp = input("Enter a string to reverse:\n")
-        print(backward_string(inp))
+        if(inp != ""):
+            i = int(input("How many times should it be repeated?\n"))
+            print(backward_string(inp))
+            print(multiply_string(inp, i))
 
     print("Good bye")
