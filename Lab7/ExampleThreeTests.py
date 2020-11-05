7f1709f1f6dce04c8e4378697745fee7431b3f2e
cassLaffan <poisonberryvines@gmail.com>
2020-11-05 11:53:32 -0500

    Fixing an error

--- Lab7/ExampleThreeTests.py
+++ Lab7/ExampleThreeTests.py
@@ -5,8 +5,10 @@ class my_tests(unittest.TestCase):
     def test1(self):
         car_one = ExampleThree.car("Honda Civic", "Red", 14000)
         car_two = ExampleThree.car("Toyota RAV4", "Green", 14000)
-        car_three = ExampleThree.car("Chrysler Malibu", "Blue", 9000)
         self.assertEqual(ExampleThree.compare_price(car_one, car_two), True)
+    def test2(self):
+        car_three = ExampleThree.car("Chrysler Malibu", "Blue", 9000)
+        car_one = ExampleThree.car("Honda Civic", "Red", 14000)
         self.assertEqual(ExampleThree.compare_price(car_one, car_three), False)
 
 if __name__ == '__main__':
