8df8a513536b8bec07bb86caf2588621912e8a0c
cassLaffan <poisonberryvines@gmail.com>
2020-11-11 20:33:37 -0500

    Adding stuff

--- Lab8/Card.py
+++ Lab8/Card.py
@@ -1,12 +1,25 @@
 class Card:
-    def __init__(self, value, color):
+    def __init__(self, value, colour):
         self.value = value
-        self.color = color
+        self.colour = colour
 
+'''
+This syntax below is called list comprehension!
+What this does puts a loop inside a list.
+
+nums = [Card(value, colour) for value in range(1, 11) for colour in colours]
+can be written as:
+
+for value in range(1, 11):
+    for colour in colours:
+        nums.append(Card(value, colour))
+
+See? It makes an object then appends it to the list!
+'''
 def createDeck():
-    colors = ['heart', 'diamonds', 'spades', 'clubs']
-    nums = [Card(value, color) for value in range(1, 11) for color in colors]
-    face = [Card(10, color) for value in range(0, 3) for color in colors]
+    colours = ['heart', 'diamonds', 'spades', 'clubs']
+    nums = [Card(value, colour) for value in range(1, 11) for colour in colours]
+    face = [Card(10, colour) for value in range(0, 3) for colour in colours]
     nums.extend(face)
 
     return nums
