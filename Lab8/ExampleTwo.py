a46b214fb35ef588ebc29ca465b6908daf1386f6
cassLaffan <poisonberryvines@gmail.com>
2020-11-11 21:32:51 -0500

    Adding examples

---
+++ Lab8/ExampleTwo.py
@@ -0,0 +1,19 @@
+import random
+
+'''
+Let's use random to create a miltidimensional array (list)
+'''
+if __name__ == "__main__":
+    # This is necessary. You can't get random numbers
+    # without seeding it first
+    random.seed()
+
+    our_dimension = random.randint(0, 10)
+    our_matrix = []
+
+    # It's really just nested lists, I promise
+    for d in range(our_dimension):
+        our_matrix.append([])
+        for i in range(our_dimension):
+            our_matrix[-1].append(random.randint(-10, 10))
+        print(our_matrix[-1])
