105902920a3f32990f5d4f2df4760bba42665375
cassLaffan <poisonberryvines@gmail.com>
2020-11-26 12:25:46 -0500

    adding another example

---
+++ Lab9/ExampleTwo.py
@@ -0,0 +1,23 @@
+import random
+
+def recurse_fruit(fruits, fruity_dict):
+    
+    if len(fruits) == 1:
+        fruity_dict[fruits[0]] = random.randint(0, 20)
+    else:
+        fruity_dict[fruits[0]] = random.randint(0, 20)
+        recurse_fruit(fruits[1:], fruity_dict)
+    
+    return fruity_dict
+
+if __name__ == "__main__":
+    
+    fruit_list = []
+    for i in range(0, 5):  
+        fruit_list.append(input("Please enter a fruit: "))
+    
+    fruit_dict = {}
+
+    fruit_dict = recurse_fruit(fruit_list, fruit_dict)
+
+    print(fruit_dict)
