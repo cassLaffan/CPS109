d6f7e06b8cd27f23114e39c31fe9a6a897e0ff39
cassLaffan <poisonberryvines@gmail.com>
2021-10-21 07:58:17 -0400

    Switching up the question

--- Lab5/ExampleFive.py
+++ Lab5/ExampleFive.py
@@ -1,9 +1,11 @@
 def print_recursively(num):
     if num == 0:
         print("Starting to print at 0")
+        return 0
     else:
-        print_recursively(num - 1)
-    print("Now we are at ", num)
+        a = print_recursively(num - 1)
+        print("Now we are at ", num)
+        return num
 
 if __name__ == "__main__":
     inp = int(input("What number of recursions would you like to see?"))
