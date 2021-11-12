3da35dcc00f84d3026db731307af0515ef789c01
cassLaffan <poisonberryvines@gmail.com>
2021-11-12 15:19:13 -0500

    Adding this week's slides

--- Lab7/ExampleOne.py
+++ Lab7/ExampleOne.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 
     our_file = open("a_file.txt", "r") # r means you "read from it"
 
-    st = our_file.read(250) # Notice we include how many characters we want to read in?
+    st = our_file.read() # Notice we include how many characters we want to read in?
     print("The file reads: ", st)
 
     our_file.close()
