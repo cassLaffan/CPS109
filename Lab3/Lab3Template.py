553669f6c85e0e32810346f3247a1a6e06085cf5
cassLaffan <poisonberryvines@gmail.com>
2022-09-26 15:28:45 -0400

    Adding the lab 3 template

---
+++ Lab3/Lab3Template.py
@@ -0,0 +1,58 @@
+# Lab 3 template file
+# Name:
+# Student ID: 
+## CRITICAL: You MUST submit a .txt file, or the maximum mark you will get is 60%!!!!!!!
+# To convert this to a .txt, you can just copy the file contents and paste into something like Notepad (Windows), or TextEdit (Mac), or Nano (Linux), then Save As a .txt
+
+def question_1(): # Have a method for each question, like so
+    # You don't need to add a docstring / function description, but it may be useful for you
+    """
+    Code used to tell user whether their input of an integer is 
+    either zero or positive or negative
+    """
+    number = int(input("Write a number: "))
+    if number == 0:
+        print ("zero")
+    # Cont...
+
+    # You don't have to return anything in these methods - you can just print.
+
+def question_2():
+    pass # Replace this "pass" with whatever your logic is for the question. 
+    #Pass just says to pass over the function (no operation).
+
+def question_3():
+    pass
+
+def question_4():
+    pass
+  
+def question_5():
+    pass
+
+def question_6():
+    pass
+
+def question_7():
+    pass
+    
+def question_8():
+    pass
+    
+def question_9():
+    pass
+    
+def question_10():
+    pass
+
+if __name__ == "__main__":
+    question_1() # You can comment any of these out for testing purposes
+    question_2()
+    question_3()
+    question_4()
+    question_5()
+    question_6()
+    question_7()
+    question_8()
+    question_9()
+    question_10()
