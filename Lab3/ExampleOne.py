11949b7d03390d26ffe824a6d20b3ab8f1c0ac6d
cassLaffan <poisonberryvines@gmail.com>
2022-09-26 15:19:44 -0400

    Rearranging, updating

--- Lab3/ExampleOne.py
+++ Lab3/ExampleOne.py
@@ -1,37 +1,16 @@
-# Let's do a basic example of exhaustive enumeration
-# (also known as brute force!) through a very basic list.
+# This example shows us two things:
+# First, that you can call a function within a for loop (very important!)
+# and second, the syntax for iterating over a range of numbers in python.
 
-
-# What this function will do is take two arguments:
-# First, a list, then the integer you want to find in the list
-# it will return a list of the indicies where new_num occurs.
-def check_every_item(my_list, new_num):
-
-    # Let's define an empty list that we can append our indicies to.
-    ind_list = []
-
-    i = 0
-
-    while i < len(my_list):
-        if my_list[i] == new_num:
-            # Lists, unlike strings, can be modified!
-            # So we just add new items to
-            ind_list.append(i)
-        # i = i + 1
-        i+=1
-
-    return ind_list
+# Notice how this function has no return statement, but a print statement?
+# Python functions treat print statements as return statements if no other return
+# statements exist in the function.
+def count(num):
+    print(num + 1)
 
 if __name__ == "__main__":
-    # Here we will define a list in our program.
-    # We will have some examples later on that will have more
-    # dynamic lists made from user input.
-
-    # The numbers are intentional
-    # Indicies are 0 - 7, where 7 is the length of the list minus
-    new_list = [2, 5, 1, 6, 2, 82, -10, 2]
-
-    index_list = check_every_item(new_list, 2)
-    # The output for this program will be [0, 4, 7]
-    # Meaning we had to iterate through every single element to find every 2.
-    print(index_list)
+    # This is pretty intuitive! All it does is iterate over the numbers
+    # 1 to 10 and feeds them into our count function. You'll notice that
+    # the computer knows to increment n by 1 for every pass of the loop.
+    for n in range(10):
+        count(n)
