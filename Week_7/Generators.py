d540532f1a82d633eb5eb765fee8b29f2bae37b0
cassLaffan <poisonberryvines@gmail.com>
2024-11-06 09:53:38 -0500

    Adding last night's examples

--- Week_7/Generators.py
+++ Week_7/Generators.py
@@ -6,7 +6,7 @@ execute right away. Instead, they return iterator objects.
 def count_up_to(n):
     count = 1
     while count <= n:
-        # print("Hello")
+        print("Hello")
         yield count
         count += 1
 
@@ -14,10 +14,23 @@ def lazy_range(n):
     for i in range(n):
         yield i
 
+def backwards_lazy_range(lower, upper):
+    for i in range(upper, lower - 1, -1):
+        yield i
+
 if __name__ == "__main__":
     counter = count_up_to(5)
     for num in counter:
         print(num)
-
+    '''
     for num in lazy_range(5):  # Generates numbers 0 to 4 on-demand
         print(num)
+    '''
+
+    upper = int(input("What is the upper limit? "))
+    lower = int(input("What is the lower limit? "))
+
+    lr = backwards_lazy_range(lower, upper)
+
+    for item in lr:
+        print(item)
--- Week_7/Lambda_Example.py
+++ Week_7/Lambda_Example.py
@@ -4,8 +4,14 @@ function.
 '''
 if __name__ == "__main__":
     sum_of_squares = lambda x, y: x*x + y*y
-    print(sum_of_squares(3, 4)) 
+    print(sum_of_squares(3, 8)) 
 
-    data = [{'name': 'Charlie', 'age': 30}, {'name': 'Alice', 'age': 25}]
+    data = [{'name': 'Charlie', 'age': 30}, {'name': 'Alice', 'age': 25},
+            {'name': 'Jesse', 'age': 18}]
+    '''
+    ret = lambda x, y: x[y]['age']
+    for item in range(len(data)):
+        print(ret(data, item))
+    '''
     sorted_data = sorted(data, key=lambda x: x['age'])  # Sort by age
     print(sorted_data)
