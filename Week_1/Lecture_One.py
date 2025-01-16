e317d40cd4b65f1a636012c8d9b56a09f664b6f7
cassLaffan <poisonberryvines@gmail.com>
2025-01-15 21:27:05 -0500

    Adding today's example code

--- Week_1/Lecture_One.py
+++ Week_1/Lecture_One.py
@@ -1,41 +1,24 @@
-ex_one = 7 + (2 * 3) ** 2 / 4
-print("7 + (6) ** 2 / 4")
-print("7 + 36 / 4")
-print("7 + 9.0")
-print("16.0")
+# Creating our first variable and storing the result of
+# this mathematical operator!
+variable_one = 7 + (2 * 3) ** 2 / 4
+variable_two = variable_one + 10
+int_divison = 9//4
+big_operation = (8 + 2) * (5 - 3) ** 2
+string_add = "String one and " + "string two!"
 
-ex_two = 9 // 4
-ex_three = 9 / 4
+# Invoking the print statement! This shows output
+# to our user!
+print("Hello!")
+# We can print off variable values
+print(variable_one)
+print(variable_two)
+print(int_divison)
+print(big_operation)
+# We can print off the result of operations directly!
+print(9/4)
 
-ex_four = (8 + 2) * (5 - 3) ** 2
-print("(10) * (2) ** 2")
-print("(10) * 4")
-print("40")
-
-print("Our first value is: ", ex_one)
-print("Our second value and third value are", ex_two, "and", ex_three)
-print("Our final, fourth value is: ", ex_four)
-
-ex_five = "This is" + " a " + "concatonated string."
-print(ex_five)
-
-ex_six = str(ex_one)
-print(ex_six)
-
-print(9 ** 3)
-
-ex_seven = int("5")
-print(ex_seven)
-
-ex_eight = True
-ex_nine = False
-
-ex_ten = True and False
-
-print(ex_ten)
-
-ex_eleven = True or False
-
-print(ex_eleven)
-
-# This is a comment
+# This is us overwriting variable_two! There is still one
+# instance of this variable. It just now has a new value.
+variable_two = "Overwritten!"
+print(variable_two)
+print(string_add)
