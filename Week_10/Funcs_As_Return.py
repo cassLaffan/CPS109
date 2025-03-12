8ae46c7d0234b17a2b3256178b7a3dad6c12340e
cassLaffan <poisonberryvines@gmail.com>
2025-03-12 18:12:59 -0400

    Moving some things around

---
+++ Week_10/Funcs_As_Return.py
@@ -0,0 +1,16 @@
+'''
+Here, we can see that you can return a function instead of
+a value in Python!
+'''
+
+def outer():
+	def inner(a_value):
+		return f"Hello from the inner function, {a_value}!"
+	# Note that we can return the function by name?
+	return inner
+
+
+if __name__ == "__main__":
+	return_func = outer()
+	# And we can call the returned function!
+	print(return_func("wow!")) 
---
+++ Week_10/Funcs_In_Structures.py
@@ -0,0 +1,16 @@
+'''
+In this file, we explore how functions can be stored in iterable
+data structures, such as lists.
+'''
+# Some basic functions
+def add(a, b):
+	return a + b
+
+def multiply(a, b):
+	return a * b
+
+if __name__ == "__main__":
+	# Storing the functions in a list
+	operations = [add, multiply]
+	# Calling the first function by index, giving it arguments
+	print(operations[0](2, 3))
